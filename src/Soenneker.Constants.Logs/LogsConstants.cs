fd5d840f68da256b709139ce569a94d415a17037
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:13 -0500

    xml commenting

--- src/Soenneker.Constants.Logs/LogsConstants.cs
+++ src/Soenneker.Constants.Logs/LogsConstants.cs
@@ -5,7 +5,13 @@
 /// </summary>
 public static class LogsConstants
 {
+    /// <summary>
+    /// The new line separator.
+    /// </summary>
     public const string NewLineSeparator = "*************************************************";
 
+    /// <summary>
+    /// The default directory.
+    /// </summary>
     public const string DefaultDirectory = "Logs";
 }
